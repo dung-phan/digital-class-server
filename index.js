401381f926d2240e0dd5b9a680c93474ea4e3380
dung-phan <madewithcrayons@gmail.com>
2019-11-04 10:31:28 +0100

    set up express and sequelize

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+const bodyParser = require('body-parser');
+//const cors = require('cors');
+const bodyParserMiddleWare = bodyParser.json();
+//const corsMiddleWare = cors();
+
+const db = require('./db');
+// const User = require('./user/model');
+// const Table = require('./table/model');
+
+// const signupRouter = require('./user/router');
+// const loginRouter = require('./auth/router');
+// const lobbyRouter = require('./table/router');
+db.sync()
+  .then(() => {
+    console.log('Database connected');
+    // const tableNames = ['Egel', 'Das', 'Eagle', 'Pinguin'];
+    // const tables = tableNames.map(tableName =>
+    //   Table.create({ name: tableName })
+    // );
+    // return Promise.all(tables);
+  })
+
+  .catch(error => console.error);
+app
+  //   .use(corsMiddleWare)
+  .use(bodyParserMiddleWare)
+  //   .use(signupRouter)
+  //   .use(loginRouter)
+  //   .use(lobbyRouter)
+  .listen(port, () => console.log('Server runing on port: ', port));
