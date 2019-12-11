7880eb851c05f6ef0097c5ac32d7e117a51ce2b3
dung-phan <madewithcrayons@gmail.com>
2019-12-11 11:27:05 +0100

    update readme file

---
+++ README.md
@@ -0,0 +1,48 @@
+## Contents of this README file:
+
+- Introduction
+- Features Overview
+- Installation
+- Authentication
+
+# Introduction
+
+This repository is the version control system of the backend version of the Digital Class app.
+This project uses Express server and Sequelize ORM.
+It has been deployed on [Heroku](https://digitalclass-server.herokuapp.com/).
+
+Link to the front-end version [here](https://github.com/dung-phan/digital-class-frontend)
+
+## Features Overview
+
+With this RESTful API, a user can:
+
+- Retrieve a list of classes, students and their evaluations
+- Add classes and new students in a class.
+- Add evaluations to students
+- Edit and remove students from a class
+
+## Installation
+
+1. Start a Postgres database in a docker container with the following command:
+
+   ```
+   $ docker run --rm -e POSTGRES_PASSWORD=pizza -p 5432:5432 postgres
+   ```
+
+To connect to the Database on a Mac you can use [Postico](https://eggerapps.at/postico/), on Linux - [DBeaver](https://dbeaver.io/)
+
+2. Install the server and run it
+
+   ```
+   $ git clone
+   $ cd ./digital-class-server
+   $ npm install
+   $ node . or nodemon .
+   ```
+
+3. Set up the front-end. The repo including instructions can be found [here](https://github.com/dung-phan/digital-class-frontend).
+
+## Authentication
+
+Codes for authentication are all available in the repo but not implemented for the purpose of demonstration.
--- index.js
+++ index.js
@@ -13,123 +13,13 @@ const evaluationRouter = require("./evaluation/router");
 const userRouter = require("./user/router");
 const authRouter = require("./auth/router");
 
-//models and db
-
-const Batch = require("./batch/model");
-const Student = require("./student/model");
-const Evaluation = require("./evaluation/model");
+//db
 const db = require("./db");
 
 //init
 const app = express();
 const port = process.env.PORT || 4000;
-db.sync()
-
-  // .then(() => {
-  //   const students = [
-  //     {
-  //       name: "Ned Stark",
-  //       batchId: 1,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/5/52/Ned_Stark-Sean_Bean.jpg/220px-Ned_Stark-Sean_Bean.jpg"
-  //     },
-  //     {
-  //       name: "Jaime Lannister",
-  //       batchId: 2,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Jaime_Lannister-Nikolaj_Coster-Waldau.jpg/220px-Jaime_Lannister-Nikolaj_Coster-Waldau.jpg"
-  //     },
-  //     {
-  //       name: "Cersei Lannister",
-  //       batchId: 3,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/2/22/Cersei_Lannister_in_Black_Dress_in_Season_5.jpg/220px-Cersei_Lannister_in_Black_Dress_in_Season_5.jpg"
-  //     },
-  //     {
-  //       name: "Daenerys Targaryen",
-  //       batchId: 4,
-  //       photo:
-  //         "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2F736x%2F46%2Fdc%2F52%2F46dc527a545ce51e4101d5825771f630.jpg&f=1&nofb=1"
-  //     },
-  //     {
-  //       name: "Jorah Mormont",
-  //       batchId: 1,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Jorah_Mormont-Iain_Glen.jpg/220px-Jorah_Mormont-Iain_Glen.jpg"
-  //     },
-  //     {
-  //       name: "Jon Snow",
-  //       batchId: 2,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Jon_Snow_Season_8.png/220px-Jon_Snow_Season_8.png"
-  //     },
-  //     {
-  //       name: "Sansa Stark",
-  //       batchId: 3,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/SophieTurnerasSansaStark.jpg/220px-SophieTurnerasSansaStark.jpg"
-  //     },
-  //     {
-  //       name: "Arya Stark",
-  //       batchId: 4,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/3/39/Arya_Stark-Maisie_Williams.jpg/220px-Arya_Stark-Maisie_Williams.jpg"
-  //     },
-  //     {
-  //       name: "Theon Greyjoy",
-  //       batchId: 1,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Theon_Greyjoy-Alfie_Allen.jpg/220px-Theon_Greyjoy-Alfie_Allen.jpg"
-  //     },
-  //     {
-  //       name: "Bran Stark",
-  //       batchId: 2,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/f/fa/Bran_Stark_-_Isaac_Hempstead-Wright.jpeg/220px-Bran_Stark_-_Isaac_Hempstead-Wright.jpeg"
-  //     },
-  //     {
-  //       name: "Joffrey Baratheon",
-  //       batchId: 3,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Joffrey_Baratheon-Jack_Gleeson.jpg/220px-Joffrey_Baratheon-Jack_Gleeson.jpg"
-  //     },
-  //     {
-  //       name: "Davos Seaworth",
-  //       batchId: 4,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/9/98/Davos_Seaworth_Sitting_Liam_Cunningham.jpg/220px-Davos_Seaworth_Sitting_Liam_Cunningham.jpg"
-  //     },
-  //     {
-  //       name: "Tyrion Lannister",
-  //       batchId: 1,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Tyrion_Lannister-Peter_Dinklage.jpg/220px-Tyrion_Lannister-Peter_Dinklage.jpg"
-  //     },
-  //     {
-  //       name: "LittleFinger",
-  //       batchId: 2,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Aidan_Gillen_playing_Petyr_Baelish.jpg/220px-Aidan_Gillen_playing_Petyr_Baelish.jpg"
-  //     },
-  //     {
-  //       name: "Sam",
-  //       batchId: 3,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/4/4d/Samwell_Tarly-John_Bradley.jpg/220px-Samwell_Tarly-John_Bradley.jpg"
-  //     },
-  //     {
-  //       name: "Varys",
-  //       batchId: 4,
-  //       photo:
-  //         "https://upload.wikimedia.org/wikipedia/en/thumb/0/01/Varys-Conleth_Hill.jpg/220px-Varys-Conleth_Hill.jpg"
-  //     }
-  //   ];
-
-  //   const studentPromises = students.map(student => Student.create(student));
-  //   return Promise.all(studentPromises);
-  // })
-
-  .catch(console.error);
+db.sync().catch(console.error);
 app
   .use(corsMiddleWare)
   .use(bodyParserMiddleWare)
